35ff8ba7b880088c77e8ff498401a3388b940d85
Claas <claas.moehlmann@outlook.com>
2023-06-07 10:30:06 +0000

    Use tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./webapp/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),],
+}
+
