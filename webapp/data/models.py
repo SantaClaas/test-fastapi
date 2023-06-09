29374b12863dee86116b8da7c8a4a64a7e5045a3
Claas <claas.moehlmann@outlook.com>
2023-06-09 12:41:37 +0200

    Add potential ideas to expand apps

--- webapp/data/models.py
+++ webapp/data/models.py
@@ -15,6 +15,7 @@ class App(Base):
     __tablename__ = "apps"
 
     # id used by us is equal to the app host currently
+    # TODO It would be ideal to use same as https://w3c.github.io/manifest/#dfn-identity
     id = Column(String, primary_key=True, index=True)
     # TODO consider refactor to proper normalization as id is part of manifest url
     # Manifest url is not part of the manifest but for us to keep a reference to the orignal source
@@ -32,6 +33,17 @@ class App(Base):
     # Technically also a limited set we should check but don't
     categories = relationship(
         "Category", back_populates="apps", secondary=app_category)
+    # direction = Column(String, default="auto")
+    # # The language of the manifests localizable members
+    # language = Column(String, nullable=True)
+    # short_name = Column(String, nullable=True)
+    # scope = Column(String, nullable=True)
+    # scope = Column(String, nullable=True)
+    # display = Column(String, nullable=True)
+    # orientation = Column(String, nullable=True)
+    # start_url = Column(String, nullable=True)
+    # theme_color = Column(String, nullable=True)
+    # background_color = Column(String, nullable=True)
 
 # See https://developer.mozilla.org/en-US/docs/Web/Manifest/icons and https://w3c.github.io/manifest/#icons-member and
 # https://w3c.github.io/manifest/#manifest-image-resources and https://www.w3.org/TR/image-resource/#dfn-image-resource
@@ -39,7 +51,6 @@ class App(Base):
 
 class Icon(Base):
     __tablename__ = "icons"
-
     id = Column(Integer, primary_key=True, index=True)
     app_id = Column(String, ForeignKey("apps.id"))
     source = Column(String)
