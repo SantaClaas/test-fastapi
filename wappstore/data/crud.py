448557cfaf4301c551b84a949119d8d2b6b215ed
Claas <claas.moehlmann@outlook.com>
2023-06-22 19:19:59 +0200

    Add documentation

--- wappstore/data/crud.py
+++ wappstore/data/crud.py
@@ -4,21 +4,32 @@ from typing import Iterable
 from . import models
 
 
-# TODO implement pagination when app list gets too big
 def get_apps(session: Session):
+    """
+    Gets all apps saved in the the database
+    """
     return session.query(models.App)
 
 
 def get_app(session: Session, app_id: str):
+    """
+    Gets an app by its id from the database
+    """
     return session.query(models.App).filter(models.App.id == app_id).first()
 
 
 def create_app(session: Session, app: models.App):
+    """
+    Creates an app in the database
+    """
     session.add(app)
     session.commit()
 
 
 def delete_app(session: Session, app_id: str):
+    """
+    Delets an app by id in the database
+    """
     app = session.query(models.App).filter_by(id=app_id).first()
     session.delete(app)
 
@@ -26,4 +37,7 @@ def delete_app(session: Session, app_id: str):
 
 
 def get_categories(session: Session):
+    """
+    Gets all existing app categories in the database
+    """
     return session.query(models.Category)
--- wappstore/data/database.py
+++ wappstore/data/database.py
@@ -4,20 +4,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# All the things for setting up the database
 SQLALCHEMY_DATABASE_URL = "sqlite:///wapp.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
 
 @event.listens_for(Engine, "connect")
-def set_sqlite_pragma(dbapi_connection, connection_record):
-    # Cascade does not work without this
+def enable_cascade(dbapi_connection, connection_record):
+    """"
+    Cascade does not work without this and we need it for auto deleting icons and screenshots
+    """
     cursor = dbapi_connection.cursor()
     cursor.execute("PRAGMA foreign_keys=ON")
     cursor.close()
